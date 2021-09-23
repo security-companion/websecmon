064013f1c17ccca86b8ef6fe7fdbb6485f709171
J. Mammele <jomammele@web.de>
2021-09-23 22:49:51 +0200

    move templates, commit db.sqlite3

--- scanassets/views.py
+++ scanassets/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def scanassets(request):
-    return render(request, 'scanassets.html')
+    return render(request, 'scanassets/scanassets.html')
 
 def scanasset(request, pk):
-    return render(request, 'single-scanasset.html')
+    return render(request, 'scanassets/single-scanasset.html')
