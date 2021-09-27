02a7b855c0c25fc42da7fa4e49cc7ebd96db8a99
J. Mammele <jomammele@web.de>
2021-09-27 23:00:44 +0200

    add dynamic content

--- scanassets/urls.py
+++ scanassets/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('scanassets/', views.scanassets, name="scanasets"),
-    path('scanasset/<str:pk>', views.scanasset, name="scanaset"),
+    path('', views.scanassets, name="scanasets"),
+    path('scanasset/<str:pk>', views.scanasset, name="scanasset"),
 ]
--- scanassets/views.py
+++ scanassets/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+scanassetsList =[
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'desc1'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'desc2'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'desc3'
+    },
+]
+
+
 def scanassets(request):
-    return render(request, 'scanassets/scanassets.html')
+    msg = 'scanassets'
+    number = 10
+    context = {'message': msg, 'number': number, 'scanassets': scanassetsList }
+    return render(request, 'scanassets/scanassets.html', context)
 
 def scanasset(request, pk):
-    return render(request, 'scanassets/single-scanasset.html')
+    scanassetObj = None
+    for i in scanassetsList:
+        if i['id'] == pk:
+            scanassetObj = i
+    return render(request, 'scanassets/single-scanasset.html', {'scanasset': scanassetObj})
