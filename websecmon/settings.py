ee95dceb92bb859c5f79f9b8c9f3493d764e1e99
J. Mammele <jomammele@web.de>
2021-09-27 22:23:54 +0200

    use pyton decouple

--- websecmon/settings.py
+++ websecmon/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,11 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+# See https://www.ordinarycoders.com/blog/article/secure-django-variables-with-python-decouple
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!di&^r4t7wl%f_e*n=&%bk&s$-^wg2#&1w)b_zc+9vnj=)#)&l'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
